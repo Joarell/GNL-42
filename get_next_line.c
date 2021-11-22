fc1cdc69584da35884391ce6a1528b97851cc61b
Joarell <josephjoarell@gmail.com>
2021-11-22 00:02:08 -0300

    GNL, 40%. 22-NOV-00:02

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,53 @@
 /*   By: Jev <jsouza-c@student.42sp.org.br>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 23:28:57 by Jev               #+#    #+#             */
-/*   Updated: 2021/11/20 21:14:09 by Jev              ###   ########.fr       */
+/*   Updated: 2021/11/21 23:52:55 by Jev              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/* Copiar a quantidade de bites mediante o BUFFER_SIZE.
+ * Retornar a linha mediante o \n, encontrado
+ * Caso não tenha nada para ser lido, retorne (NULL)
+ *
+ */
 #include "get_next_line.h"
+
+static t_list	*new_line(int fd)
+{
+	t_list	*new_l;
+
+	return (new_l);
+}
+static void	lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*tmp;
+
+	tmp = *lst;
+	if (new == NULL)
+		return ;
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
+	while (tmp->next)
+	{
+		tmp = tmp->next;
+	}
+}
 
 char	*get_next_line(int fd)
 {
-	static t_list	*lines;
+	static	t_list	*lines;
+	char	*file;
+	
+	
+	file = (char *)malloc(BUFFER_SIZE * sizeof(char));
+	if (file == NULL)
+		return (NULL);
+	read(fd, file, BUFFER_SIZE);
 
-	return (0);
+	return (NULL);
 }
 
 #include <stdio.h>
