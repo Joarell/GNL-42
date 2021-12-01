77b9df28d5c05d641388e57b37951ef484aed378
Joarell <josephjoarell@gmail.com>
2021-12-01 01:16:41 -0300

    GNL, updated and tuned. fix leaks needed. Dec-01-01:16

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,13 @@
 /*   By: Jev <jsouza-c@student.42sp.org.br>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 23:28:57 by Jev               #+#    #+#             */
-/*   Updated: 2021/11/29 02:20:24 by Jev              ###   ########.fr       */
+/*   Updated: 2021/12/01 01:10:38 by Jev              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static t_list	*g_lst;
 
 static	char	*move_and_creat(char const *fd)
 {
@@ -20,11 +22,13 @@ static	char	*move_and_creat(char const *fd)
 
 	i = 0;
 	w = 0;
-	while (fd[i - 1] != '\n' && i <= BUFFER_SIZE && fd[i] != '\0')
+	line = NULL;
+	while (fd[i] != '\n' && i <= BUFFER_SIZE && fd[i] != '\0')
 	{
 		i++;
 	}
-	line = (char *)malloc(i * sizeof(char));
+	i++;
+	line = (char *)malloc(i * sizeof(char) + 1);
 	if (line == NULL)
 		return (NULL);
 	while (i--)
@@ -55,68 +59,74 @@ static	t_list	*nodes(t_list **lst, char *line)
 	t_list	*aux;
 
 	aux = *lst;
-	new_line = NULL;
 	new_line = (t_list *)malloc(sizeof(t_list));
 	if (new_line == NULL)
 	{
 		return (NULL);
 	}
-	*(char *)new_line->content = *line;
+	new_line->content = line;
 	new_line->next = NULL;
 	if (*lst == NULL)
 	{
+		new_line->back = aux;
 		return (*lst = new_line);
 	}
 	while (aux->next)
+	{
 		aux = aux->next;
+	}
+	new_line->back = aux;
 	aux->next = new_line;
 	return (aux);
 }
 
-static	t_list	*creating_list(char *fd)
+static	char	*creating_list(char *fd)
 {
-	static t_list	*lst;
-	size_t			buffer;
+	size_t		buffer;
+	char		*new;
+	char		*def;
 
-	if (!lst)
+	buffer = BUFFER_SIZE;
+	g_lst = NULL;
+	if (!fd || !buffer)
+		return (NULL);
+	def = fd;
+	new = NULL;
+	while (buffer && *fd != '\0')
 	{
-		buffer = BUFFER_SIZE;
-		lst = NULL;
-		if (!fd || !buffer)
-			return (NULL);
-		while (buffer)
-		{
-			nodes(&lst, move_and_creat(fd));
-			buffer -= len_fd(move_and_creat(fd));
-			fd += len_fd(move_and_creat(fd));
-			if (!*fd)
-			{
-				break ;
-				free(fd);
-			}
-		}
-		return (lst->content);
+		new = move_and_creat(fd);
+		nodes(&g_lst, new);
+		buffer -= len_fd(new);
+		fd += len_fd(new);
 	}
-	free(lst->content);
-	lst = lst->next;
-	return (lst->content);
+	free(def);
+	return (g_lst->content);
 }
 
 char	*get_next_line(int fd)
 {
 	char	*file;
+	t_list	*erase;
 
-	if (BUFFER_SIZE)
+	if (BUFFER_SIZE && !g_lst)
 	{
 		file = (char *)malloc(BUFFER_SIZE * sizeof(char));
 		if (file == NULL)
 			return (NULL);
-		if (!(read(fd, file, BUFFER_SIZE)))
-		{
+		read(fd, file, BUFFER_SIZE);
+		if (*file)
+			return ((char *)creating_list(file));
+		else
 			free(file);
-			return (NULL);
-		}
-		return ((char *)creating_list(file));
+				return (NULL);
+	}
+	erase = NULL;
+	if (g_lst->next != NULL)
+	{
+		g_lst = g_lst->next;
+		erase = g_lst->back;
+		free(erase->content);
+		return (g_lst->content);
 	}
 	return (NULL);
 }
@@ -130,11 +140,12 @@ int main (void)
 
 	/* fd = open("./gnlTester/files/empty", O_RDONLY); */
 	fd = open("text.txt", O_RDONLY);
-	i = 1;
+	i = 6;
 	while(i--)
 	{
 		printf("%s", get_next_line(fd));
 	}
+	free(get_next_line(fd));
 	close(fd);
 	return (0);
 }
