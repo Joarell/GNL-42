dc71b0f0675043dbcd6e7ee1e6b35c17f874149f
josephjoarell@gmail.com <josephjoarell@gmail.com>
2022-03-07 00:21:08 -0300

    GNL, almost... I`m still trying to solve all 4 memory leacks. Almost there!`

--- get_next_line.c
+++ get_next_line.c
@@ -21,19 +21,19 @@ t_list			*g_hold;
 static char	*glue(char *fd)
 {
 	char	*complete;
-	char	*tmp;
 
-	tmp = g_lst->content;
+	g_aux = g_lst->content;
 	complete = (char *)malloc((g_len + g_buffer) * sizeof(char) + 1);
 	if (complete == NULL)
 		return (NULL);
 	g_buffer = 0;
 	while (g_len--)
 	{
-		complete[g_buffer] = tmp[g_buffer];
+		complete[g_buffer] = g_aux[g_buffer];
 		g_buffer++;
 	}
 	g_len++;
+	free(g_lst);
 	while (fd[g_len] != '\0')
 	{
 		complete[g_buffer] = fd[g_len];
@@ -48,14 +48,15 @@ static char	*glue(char *fd)
 
 static char	*nodes(char *line)
 {
+	if (!line)
+		free(g_hold);
 	if (g_lst && g_lst->next)
 	{
 		free(g_lst->content);
 		g_lst = g_lst->next;
 	}
 	g_len = 0;
-	if (line)
-		g_hold = (t_list *)malloc(sizeof(t_list));
+	g_hold = (t_list *)malloc(sizeof(t_list));
 	if (g_hold == NULL)
 		return (NULL);
 	if (g_lst == NULL)
@@ -69,39 +70,28 @@ static char	*nodes(char *line)
 		g_hold->content = glue(line);
 	}
 	g_hold->next = NULL;
-	if (g_lst)
-		free(g_lst->content);
 	g_lst = g_hold;
 	return (g_lst->content);
 }
 
 static t_list	*next_node(char *fd)
 {
-	int	i;
-
-	g_buffer = 0;
-	nodes(fd);
-	i = 0;
-	g_len = BUFFER_SIZE;
-	g_aux = (char *)malloc(g_len * sizeof(char *));
-	if (g_aux == NULL)
+	if (!fd)
 		return (NULL);
-	g_buffer = 0;
-	while (g_len--)
-	{
-		if (fd[g_buffer - i] == '\n')
-		{
-			g_aux[i] = fd[g_buffer + 1];
-			i++;
-		}
-		g_buffer++;
-	}
+	nodes(fd);
+	g_aux = fd;
+	while (*g_aux != '\n')
+		g_aux++;
+	g_aux++;
+	if (*g_aux == '\0')
+		return (NULL);
 	g_hold = (t_list *)malloc(sizeof(t_list));
 	if (g_hold == NULL)
 		return (NULL);
 	g_hold->content = g_aux;
 	g_hold->next = NULL;
 	g_lst->next = g_hold;
+	g_buffer = BUFFER_SIZE;
 	return (g_lst);
 }
 
@@ -113,11 +103,8 @@ static t_list	*creating_list(char *fd)
 	{
 		if (g_lst)
 		{
-			free(fd);
 			free(g_lst->content);
 			free(g_lst);
-			free(g_hold->content);
-			free(g_hold);
 		}
 		return (NULL);
 	}
@@ -147,13 +134,14 @@ char	*get_next_line(int fd)
 			return (NULL);
 		g_buffer = read(fd, yank, g_len);
 		creating_list(yank);
+		if (!g_buffer)
+			free(yank);
 		g_len = 0;
 		while (g_buffer && g_lst->content[g_len] != '\0')
 		{
 			g_len++;
 			if (g_lst->content[g_len] == '\n')
 				return (g_lst->content);
-
 		}
 	}
 	return (NULL);
