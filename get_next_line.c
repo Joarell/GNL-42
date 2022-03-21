920a2883f12ca886796aca1ea7a9292665bd1b81
josephjoarell@gmail.com <josephjoarell@gmail.com>
2022-03-20 21:03:27 -0300

    GNL,a change is gonna come. The actual fucntion needs to work with high buffers. I'm making some changes to handle this.

--- get_next_line.c
+++ get_next_line.c
@@ -22,18 +22,19 @@ static char	*glue(char *fd)
 {
 	char	*complete;
 
-	g_aux = g_lst->content;
+	if (g_lst)
+		g_aux = g_hold->content;
 	complete = (char *)malloc((g_len + g_buffer + 1) * sizeof(char));
 	if (complete == NULL)
 		return (NULL);
 	g_buffer = 0;
-	while (g_len--)
+	while (g_aux)
 	{
 		complete[g_buffer] = g_aux[g_buffer];
 		g_buffer++;
 	}
-	g_len++;
-    free(g_lst);
+	if (g_lst)
+		free(g_hold);
 	while (fd[g_len] != '\0')
 	{
 		complete[g_buffer] = fd[g_len];
@@ -48,16 +49,16 @@ static char	*glue(char *fd)
 
 static char	*nodes(char *line)
 {
-	if (!line)
-		free(g_hold);
 	if (g_lst && g_lst->next)
 		g_lst = g_lst->next;
 	g_len = 0;
 	g_hold = (t_list *)malloc(sizeof(t_list));
 	if (g_hold == NULL)
 		return (NULL);
-	if (g_lst == NULL)
-		g_hold->content = line;
+	if (g_lst)
+		g_lst->next = g_hold;
+	if (!g_lst)
+		g_hold->content = glue(line);
 	else
 	{
 		while (g_lst->content[g_len] != '\0')
@@ -67,31 +68,33 @@ static char	*nodes(char *line)
 		g_hold->content = glue(line);
 	}
 	g_hold->next = NULL;
+	g_hold->back = NULL;
+	if (g_lst)
+		g_hold->back = g_lst;
 	g_lst = g_hold;
-    g_len = 0;
+	g_len = 0;
 	return (g_lst->content);
 }
 
 static t_list	*next_node(char *fd)
 {
 	nodes(fd);
-    while (fd[g_len] != '\n')
-        g_len++;
-    if (g_len == 0)
-        g_len = BUFFER_SIZE;
+	while (fd[g_len] != '\n')
+		g_len++;
+	g_buffer = g_len;
+	g_len = BUFFER_SIZE - (g_len + 1);
 	g_aux = (char *)malloc((g_len + 1) * sizeof(char));
-    if (g_aux == NULL)
-        return (NULL);
-    g_len++;
-    g_buffer = 0;
-    while (fd[g_len] != '\0')
-    {
-        g_aux[g_buffer] = fd[g_len]; 
-        g_buffer++;
-        g_len++;
-    }
-    free(fd);
-    g_aux[g_buffer] = '\0';
+	if (g_aux == NULL)
+		return (NULL);
+	g_len = g_buffer + 1;
+	g_buffer = 0;
+	while (fd[g_len] != '\0')
+	{
+		g_aux[g_buffer] = fd[g_len];
+		g_buffer++;
+		g_len++;
+	}
+	g_aux[g_buffer] = '\0';
 	g_hold = (t_list *)malloc(sizeof(t_list));
 	if (g_hold == NULL)
 		return (NULL);
@@ -104,26 +107,27 @@ static t_list	*next_node(char *fd)
 static t_list	*creating_list(char *fd)
 {
 	if (!g_lst || !g_buffer)
-        g_lst = NULL;
+		g_lst = NULL;
 	if (!g_buffer)
-    {
-        if (g_hold)
-            free(g_hold);
-        if (g_aux)
-            free(g_aux);
-        free(fd);
-        return (NULL);
-    }
+	{
+		free(fd);
+		return (NULL);
+	}
 	fd[g_buffer] = '\0';
 	g_buffer = 0;
-	while (g_len--)
+	while (fd[g_buffer] != '\0')
 	{
 		if (fd[g_buffer] != '\n')
 			g_buffer++;
-		else if (fd[g_buffer] != '\0')
-            return (next_node(fd));
+		if (fd[g_buffer] == '\n')
+			nodes(fd);
+		while (fd[g_buffer] != '\0')
+		{
+			if (fd[g_buffer] != '\n')
+				next_node(fd);
+			g_buffer++;
+		}
 	}
-	nodes(fd);
 	return (g_lst);
 }
 
@@ -140,15 +144,15 @@ char	*get_next_line(int fd)
 			return (NULL);
 		g_buffer = read(fd, yank, g_len);
 		creating_list(yank);
-        if (!g_lst)
-            return (NULL);
+		if (!g_lst)
+			return (NULL);
 		g_len = 0;
 		while (g_lst->content[g_len])
 		{
 			g_len++;
 			if (g_lst->content[g_len] == '\n')
-                return (g_lst->content);
-		}
+				return (g_lst->content);
+			}
 	}
 	return (NULL);
 }
@@ -159,19 +163,19 @@ int main(void)
 {
 	int     fd;
 	int     i;
-    char    *str;
+	char    *str;
 
 	fd = open("text.txt", O_RDONLY);
 	i = 3;
 	while (i--)
 	{
-        str = get_next_line(fd);
+		str = get_next_line(fd);
 		printf("%s", str);
-        if (str != NULL)
-        {
-            free(str);
-            str = NULL;
-        }
+		if (str != NULL)
+		{
+			free(str);
+			str = NULL;
+		}
 	}
 	close(fd);
 	return (0);
