df6f3695d685c857cb0da70872066a65009b7498
Joseph Joarell <josephjoarell@gmail.com>
2022-04-03 22:48:54 -0300

    GNL, a change is gonna come.

--- get_next_line.c
+++ get_next_line.c
@@ -47,87 +47,105 @@ static char	*glue(char *fd)
 	return (complete);
 }
 
-static char	*nodes(char *line)
+static t_list	*nodes(char *line)
 {
-	if (g_lst && g_lst->next)
-		g_lst = g_lst->next;
+	t_list	*node;
+
 	g_len = 0;
-	g_hold = (t_list *)malloc(sizeof(t_list));
-	if (g_hold == NULL)
+	node = (t_list *)malloc(sizeof(t_list));
+	if (node == NULL)
 		return (NULL);
-	if (g_lst)
-		g_lst->next = g_hold;
 	if (!g_lst)
-		g_hold->content = glue(line);
+		node->content = glue(line);
 	else
 	{
 		while (g_lst->content[g_len] != '\0')
 			g_len++;
 		while (line[g_buffer] != '\0')
 			g_buffer++;
-		g_hold->content = glue(line);
+		node->content = glue(line);
 	}
-	g_hold->next = NULL;
-	g_hold->back = NULL;
-	if (g_lst)
-		g_hold->back = g_lst;
-	g_lst = g_hold;
+	node->next = NULL;
+	node->back = NULL;
 	g_len = 0;
-	return (g_lst->content);
+	return (node);
 }
 
-static t_list	*next_node(char *fd)
+static void	next_node(char *fd)
 {
-	nodes(fd);
-	while (fd[g_len] != '\n')
-		g_len++;
-	g_buffer = g_len;
-	g_len = BUFFER_SIZE - (g_len + 1);
-	g_aux = (char *)malloc((g_len + 1) * sizeof(char));
-	if (g_aux == NULL)
-		return (NULL);
-	g_len = g_buffer + 1;
-	g_buffer = 0;
-	while (fd[g_len] != '\0')
+	t_list	*cargo;
+
+	cargo = NULL;
+	if (!fd)
+		return ;
+	if (!g_lst)
+		g_lst = nodes(fd);
+	else
 	{
-		g_aux[g_buffer] = fd[g_len];
-		g_buffer++;
-		g_len++;
+		if (!g_hold)
+			g_hold = g_lst;
+		cargo = nodes(fd);
+		g_hold->next = cargo;
+		if (!g_lst->back)
+			g_hold->back = g_lst;
+		else
+			g_hold->back = g_hold;
+		g_hold = g_hold->next;
 	}
-	g_aux[g_buffer] = '\0';
-	g_hold = (t_list *)malloc(sizeof(t_list));
-	if (g_hold == NULL)
-		return (NULL);
-	g_hold->content = g_aux;
-	g_hold->next = NULL;
-	g_lst->next = g_hold;
-	return (g_lst);
+	g_lst->back = g_lst;
+    while (*fd != '\n')
+		fd++;
+	fd++;
+	return (next_node(fd));
+	/* nodes(fd); */
+    /* fd += g_buffer; */
+	/* while (fd[g_len] != '\n') */
+	/* 	g_len++; */
+	/* g_aux = (char *)malloc((g_len + 1) * sizeof(char)); */
+	/* if (g_aux == NULL) */
+	/* 	return (NULL); */
+	/* g_len = 0; */
+	/* while (fd[g_len] != '\0') */
+	/* { */
+    /*     g_aux[g_len] = fd[g_len]; */
+    /*     if (fd[g_len] == '\n') */
+    /*         break ; */
+	/* 	g_len++; */
+	/* } */
+	/* g_aux[g_buffer] = '\0'; */
+	/* g_hold = (t_list *)malloc(sizeof(t_list)); */
+	/* if (g_hold == NULL) */
+	/* 	return (NULL); */
+	/* g_hold->content = g_aux; */
+	/* g_hold->next = NULL; */
+	/* g_lst->next = g_hold; */
+	/* return (g_lst); */
 }
 
 static t_list	*creating_list(char *fd)
 {
+    /* int     count; */
+    char    *tmp;
+
+    tmp = fd;
+    /* count = 0; */
 	if (!g_lst || !g_buffer)
 		g_lst = NULL;
-	if (!g_buffer)
-	{
-		free(fd);
-		return (NULL);
-	}
 	fd[g_buffer] = '\0';
-	g_buffer = 0;
-	while (fd[g_buffer] != '\0')
-	{
-		if (fd[g_buffer] != '\n')
-			g_buffer++;
-		if (fd[g_buffer] == '\n')
-			nodes(fd);
-		while (fd[g_buffer] != '\0')
-		{
-			if (fd[g_buffer] != '\n')
-				next_node(fd);
-			g_buffer++;
-		}
-	}
+    g_buffer = 0;
+	next_node(tmp);
+	/* while (*tmp != '\0') */
+	/* { */
+	/* 	if (tmp[g_buffer] == '\n') */
+	/* 	{ */
+    /*         count = g_buffer; */
+    /*         count++; */
+	/* 		next_node(tmp); */
+    /*         g_buffer = count + g_len; */
+	/* 	} */
+	/* 	g_buffer++; */
+	/* } */
+    free(fd);
 	return (g_lst);
 }
 
@@ -152,7 +170,7 @@ char	*get_next_line(int fd)
 			g_len++;
 			if (g_lst->content[g_len] == '\n')
 				return (g_lst->content);
-			}
+		}
 	}
 	return (NULL);
 }
@@ -166,7 +184,7 @@ int main(void)
 	char    *str;
 
 	fd = open("text.txt", O_RDONLY);
-	i = 3;
+	i = 4;
 	while (i--)
 	{
 		str = get_next_line(fd);
